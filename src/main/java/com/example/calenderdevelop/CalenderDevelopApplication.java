56014c41da73f65e5457105f7930c28aaada813a
easter1201 <kite1201kr@gmail.com>
2025-05-16 10:53:54 +0900

    first commit

---
+++ src/main/java/com/example/calenderdevelop/CalenderDevelopApplication.java
@@ -0,0 +1,13 @@
+package com.example.calenderdevelop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalenderDevelopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalenderDevelopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/calenderdevelop/CalenderDevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calenderdevelop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalenderDevelopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
